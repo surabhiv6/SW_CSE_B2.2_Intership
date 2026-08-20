b5a8afd818a13a97586f4794f21f01afb9fdf905
surabhivrn6@gmail.com <surabhivrn6@gmail.com>
2026-08-20 12:36:33 +0530

    named export & default export

--- Day/Day 12/p1.js
+++ Day/Day 12/p1.js
@@ -5,3 +5,10 @@ export function add(a,b) {
 export function subtract(a,b) {
     return a-b;
 }
+import { add,subtract } from "./P1.js"; //Named export
+import multiply from "./P3.js"; //default export
+
+console.log(add(40,60));
+console.log(subtract(90,60));
+
+console.log(multiply(60,60));
